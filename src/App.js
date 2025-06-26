7b5c2346227b1cff999eeed9ee1f87514f0892b7
ssu09090 <iso99jsj@naver.com>
2025-06-26 12:43:28 +0900

    save

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import MainLayout from "./components/MainLayout";
 import Main from "./components/Main";
@@ -6,14 +6,16 @@ import Login from "./components/Login";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    //<BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Main />} />
           <Route path="login" element={<Login />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
+    //</BrowserRouter>
   );
 };
 
